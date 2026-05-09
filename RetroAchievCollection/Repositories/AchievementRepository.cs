a5f29aa16e5d776d0a584ee41c4a91fc4e6f278e
henrique <henriquefranceschini@hotmail.com>
2026-05-09 16:25:48 -0300

    Added repositories

---
+++ RetroAchievCollection/Repositories/AchievementRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RetroAchievCollection.Data;
+using RetroAchievCollection.Models;
+
+namespace RetroAchievCollection.Repositories;
+
+public class AchievementRepository
+{
+    public async Task<AchievementModel?> GetByCodeIntegration(int codeIntegration)
+    {
+        using var db = new AppDbContext();
+        return await db.Achievements.SingleOrDefaultAsync(g => g.CodeIntegration == codeIntegration);
+    }
+    
+    public async Task<List<AchievementModel>> GetAchievements(Guid gameId)
+    {
+        using var db = new AppDbContext();
+        return await db.Achievements
+            .Where(g => g.GameId == gameId)
+            .ToListAsync();
+    }
+    
+    public async Task InsertByRange(List<AchievementModel> achievements)
+    {
+        using var db = new AppDbContext();
+        db.Achievements.AddRange(achievements);
+        await db.SaveChangesAsync();
+    }
+    
+    public async Task UpdateByRange(List<AchievementModel> achievements)
+    {
+        using var db = new AppDbContext();
+        db.Achievements.UpdateRange(achievements);
+        await db.SaveChangesAsync();
+    }
+}
---
+++ RetroAchievCollection/Repositories/GameRepository.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RetroAchievCollection.Data;
+using RetroAchievCollection.Models;
+
+namespace RetroAchievCollection.Repositories;
+
+public class GameRepository
+{
+    public async Task<GameModel?> GetByCodeIntegration(int codeIntegration, bool includeAchievements = false)
+    {
+        using var db = new AppDbContext();
+        return await BuildQuery(db, includeAchievements)
+            .SingleOrDefaultAsync(g => g.CodeIntegration == codeIntegration);
+    }
+        
+    public async Task<List<GameModel>> GetConsoleGames(Guid consoleId, bool includeAchievements = false)
+    {
+        using var db = new AppDbContext();
+
+        return await BuildQuery(db, includeAchievements)
+            .Where(g => g.ConsoleId == consoleId)
+            .ToListAsync();
+    }
+    
+    public async Task<List<GameModel>> GetFavoriteGames(bool includeAchievements = false)
+    {
+        using var db = new AppDbContext();
+
+        return await BuildQuery(db, includeAchievements)
+            .Where(g => g.IsFavorite)
+            .ToListAsync();
+    }
+    
+    public async Task<int> GetConsoleGamesCount(Guid consoleId)
+    {
+        using var db = new AppDbContext();
+        return await db.Games.CountAsync(g => g.ConsoleId == consoleId);
+    }
+    
+    public async Task InsertGame(GameModel model)
+    {
+        using var db = new AppDbContext();
+        db.Games.Add(model);
+        await db.SaveChangesAsync();
+    }
+    
+    public async Task UpdateGame(GameModel model)
+    {
+        using var db = new AppDbContext();
+        db.Games.Update(model);
+        await db.SaveChangesAsync();
+    }
+    
+    private IQueryable<GameModel> BuildQuery(AppDbContext db, bool includeAchievements)
+    {
+        var query = db.Games.AsQueryable();
+
+        if (includeAchievements)
+        {
+            query = query.Include(g => g.Achievements);
+        }
+
+        return query;
+    }
+}
